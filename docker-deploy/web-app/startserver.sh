1dada9e899a2251b33420d7fda2d0b1a44bdd971
Eunbi <zhangeunbi@gmail.com>
2020-05-27 10:21:51 -0400

    Add files via upload

---
+++ docker-deploy/web-app/startserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python3 manage.py makemigrations orders
+python3 manage.py migrate orders
+python3 manage.py makemigrations users
+python3 manage.py migrate orders
+python3 manage.py migrate
+res="$?"
+while [ "$res" != "0" ]
+do
+    sleep 3;
+    python3 manage.py migrate
+    res="$?"
+done
+python3 manage.py runserver 0.0.0.0:8000
---
+++ docker-deploy/web-app/upsrunserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while [ "1"=="1" ]
+do
+    python3 ups_truck.py vcm-14632.vm.duke.edu
+    sleep 1
+done
